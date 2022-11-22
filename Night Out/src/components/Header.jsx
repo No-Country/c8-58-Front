8657266f538135770888fcd5561e701b805acd84
Federico Santoro <federicogabsantoro@gmail.com>
2022-11-22 11:44:46 -0300

    Home Fede

--- Night Out/src/components/Header.jsx
+++ Night Out/src/components/Header.jsx
@@ -17,9 +17,19 @@ function Header() {
             path="/"
             element={
               <ul className="list-none text-gray flex flex-row text-xl justify-evenly items-center">
-                <li className="p-5"><NavLink className="p-4 px-10" to="/">Home</NavLink></li>
-                <li className="p-5"><a className="p-4 rounded-full bg-gradient-to-r from-gradiante1 via-gradiante2 to-gradiante3 px-10" href="#logear">Sign In</a>
-              </li>
+                <li className="p-5">
+                  <NavLink className="p-4 px-10" to="/">
+                    Home
+                  </NavLink>
+                </li>
+                <li className="p-5">
+                  <a
+                    className="p-4 rounded-full bg-gradient-to-r from-gradiante1 via-gradiante2 to-gradiante3 px-10"
+                    href="#logear"
+                  >
+                    Sign In
+                  </a>
+                </li>
               </ul>
             }
           ></Route>
@@ -27,9 +37,19 @@ function Header() {
             path="/Sign-up"
             element={
               <ul className="list-none text-gray flex flex-row text-xl justify-evenly items-center">
-                <li className="p-5"><NavLink className="p-4 px-10" to="/">Home</NavLink></li>
-                <li className="p-5"><NavLink className="p-4 rounded-full bg-gradient-to-r from-gradiante1 via-gradiante2 to-gradiante3 px-10" to="/Sign-In">Sign In</NavLink>
-              </li>
+                <li className="p-5">
+                  <NavLink className="p-4 px-10" to="/">
+                    Home
+                  </NavLink>
+                </li>
+                <li className="p-5">
+                  <NavLink
+                    className="p-4 rounded-full bg-gradient-to-r from-gradiante1 via-gradiante2 to-gradiante3 px-10"
+                    to="/Sign-In"
+                  >
+                    Sign In
+                  </NavLink>
+                </li>
               </ul>
             }
           ></Route>
@@ -37,22 +57,51 @@ function Header() {
             path="/Sign-In"
             element={
               <ul className="list-none text-gray flex flex-row text-xl justify-evenly items-center">
-                <li className="p-5"><NavLink className="p-4 px-10" to="/">Home</NavLink></li>
-                <li className="p-5"><NavLink className="p-4 rounded-full bg-gradient-to-r from-gradiante1 via-gradiante2 to-gradiante3 px-10" to="/Sign-In">Sign In</NavLink>
-              </li>
+                <li className="p-5">
+                  <NavLink className="p-4 px-10" to="/">
+                    Home
+                  </NavLink>
+                </li>
+                <li className="p-5">
+                  <NavLink
+                    className="p-4 rounded-full bg-gradient-to-r from-gradiante1 via-gradiante2 to-gradiante3 px-10"
+                    to="/Sign-In"
+                  >
+                    Sign In
+                  </NavLink>
+                </li>
               </ul>
             }
           ></Route>
-          <Route path="/:ruta" element={
-            <ul className="list-none text-gray flex flex-row text-xl justify-evenly items-center">
-            <li className="p-5"><form className="flex flex-row bg-gray p-2 rounded-full">
-            <img className="w-10 mr-3" src="src\assets\lupa.svg" alt="lupa" />
-              <input type="search" className="bg-gray outline-none text-black"/>
-              </form> </li>
-            <li className="p-5"><button ><img src="src\assets\menuBlanco.png" alt="menu" className="w-10"/></button>
-          </li>
-          </ul>
-          }></Route>
+          <Route
+            path="/:ruta"
+            element={
+              <ul className="list-none text-gray flex flex-row text-xl justify-evenly items-center">
+                <li className="p-5">
+                  <form className="flex flex-row bg-gray p-2 rounded-full">
+                    <img
+                      className="w-10 mr-3"
+                      src="src\assets\lupa.svg"
+                      alt="lupa"
+                    />
+                    <input
+                      type="search"
+                      className="bg-gray outline-none text-black"
+                    />
+                  </form>{" "}
+                </li>
+                <li className="p-5">
+                  <button>
+                    <img
+                      src="src\assets\menuBlanco.png"
+                      alt="menu"
+                      className="w-10"
+                    />
+                  </button>
+                </li>
+              </ul>
+            }
+          ></Route>
         </Routes>
       </nav>
     </header>
