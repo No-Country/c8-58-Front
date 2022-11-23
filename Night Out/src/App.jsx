de7e1e512e145949bd187a18f4362d2713181ca3
Santiago Rivanegra <santiagorivanegra@hotmail.com>
2022-11-23 11:52:06 -0300

    language & alerts

--- Night Out/src/App.jsx
+++ Night Out/src/App.jsx
@@ -10,51 +10,55 @@ import Publications from './components/Feed/Publications'
 import Navbar from './components/Feed/Navbar'
 import Filters from './components/Feed/Filters'
 
+import { AlertsProvider } from './components/alerts/Alerts'
+
 function App() {
   return (
     <div className="bg-fondo ">
+      <AlertsProvider>
       <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+              <main className="w-10/12 flex flex-col items-center mx-auto ">
+                <Introduction />
+                <HowItWorks />
+                <SignIn />
+              </main>
+        <Footer />
+        </>
+            }
+          ></Route>
+          <Route
+            path="/Sign-up"
+            element={
+              <>
+              <main className="w-10/12 flex flex-col items-center mx-auto ">
+                <SignUp />
+              </main>
+        <Footer />
+              </>
+            }
+          ></Route>
+          <Route path="/Sign-In" element={
             <>
             <main className="w-10/12 flex flex-col items-center mx-auto ">
-              <Introduction />
-              <HowItWorks />
-              <SignIn />
+              <LogIn />
             </main>
-      <Footer />
-      </>
-          }
-        ></Route>
-        <Route
-          path="/Sign-up"
-          element={
-            <>
-            <main className="w-10/12 flex flex-col items-center mx-auto ">
-              <SignUp />
-            </main>
-      <Footer />
+        <Footer />
             </>
-          }
-        ></Route>
-        <Route path="/Sign-In" element={
-          <>
-          <main className="w-10/12 flex flex-col items-center mx-auto ">
-            <LogIn />
-          </main>
-      <Footer />
-          </>
-        }></Route>
-        <Route path="/Feed" element={
-          <main className="w-4/5 flex flex-col items-center mx-auto relative overflow-hidden">
-          <Publications />
-          <Filters />
-          <Navbar />
-          </main>
-        }></Route>
-      </Routes>
+          }></Route>
+          <Route path="/Feed" element={
+            <main className="w-4/5 flex flex-col items-center mx-auto relative overflow-hidden">
+            <Publications />
+            <Filters />
+            <Navbar />
+            </main>
+          }></Route>
+        </Routes>
+      </AlertsProvider>
     </div>
   );
 }
--- Night Out/src/main.jsx
+++ Night Out/src/main.jsx
@@ -7,12 +7,36 @@ import { BrowserRouter } from "react-router-dom";
 import { store } from './redux/store/store'*/}
 import ScrollToTop from './components/ScrollToTop'
 
+import { I18nextProvider } from 'react-i18next'
+import i18next from 'i18next'
+
+import global_es from './translations/es/global.json'
+import global_en from './translations/en/global.json'
+
+const idioma = localStorage.getItem('language')
+
+i18next.init({
+  interpolation: {escapeValue: false},
+  lng: idioma,
+  fallbackLng: 'en',
+  resources:{
+    es: {
+      global: global_es
+    },
+    en: {
+      global: global_en
+    }
+  }
+})
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <ScrollToTop />
-    {/* <Provider store = {store}> */}
-      <App />
-    {/* </Provider> */}
+    <I18nextProvider i18n={i18next}>
+      <ScrollToTop />
+      {/* <Provider store = {store}> */}
+        <App />
+      {/* </Provider> */}
+    </I18nextProvider>  
   </BrowserRouter>
 );
 
